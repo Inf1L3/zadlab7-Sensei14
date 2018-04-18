07784960697bbc2e9d8b54ff6012835732408cca
student <student@pc-108-21>
2018-04-18 17:36:41 +0200

    Laboratoria

---
+++ OOPL7/src/main/java/pl/edu/ur/oopl7/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl7;
+
+/**
+ *
+ * @author student
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+         Student s = new Student(1, "In", "Inf", 3, "Jan", "Kowalski", 1997, "M");
+        
+       
+        System.out.println(s.toString());
+        
+    }
+    
+}
